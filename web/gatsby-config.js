03a4d3263507096ce4f502faac424b803016b399
Stu Finn <stu@borealisweb.ca>
2020-12-19 23:17:24 -0600

    npm update and remove google analytics for now

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -47,19 +47,7 @@ module.exports = {
     //   },
     // },
     `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    }, // this (optional) plugin enables Progressive Web App + Offline functionality // To learn more, visit: https://gatsby.dev/offline
+     // this (optional) plugin enables Progressive Web App + Offline functionality // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
   ],
 }
