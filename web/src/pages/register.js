fb250e851c8bd1cdad4cd2e0ddea96157041265a
Stu Finn <stu@borealisweb.ca>
2020-12-22 23:40:52 -0600

    Registration page improvements

--- web/src/pages/register.js
+++ web/src/pages/register.js
@@ -24,7 +24,7 @@ const ExternalLink = ({
     href={`${href}`}
     target="_blank"
     rel="noopener noreferrer"
-    className={`text-blue-700 underline hover:text-indigo-700 ${className}`}
+    className={` underline hover:font-bold ${className}`}
   >
     {children}
   </a>
@@ -32,6 +32,14 @@ const ExternalLink = ({
 
 const Emph = ({ children, className = "" }) => (
   <div className={`font-bold mb-2 ${className}`}>{children}</div>
+)
+
+const PageSection = ({ children, className = "" }) => (
+  <div className={` ${className}`}>
+    <div className={`flex mx-10 justify-center`}>
+      <div className="lg:w-1/2">{children}</div>
+    </div>
+  </div>
 )
 
 const Register = ({ data }) => {
@@ -43,80 +51,96 @@ const Register = ({ data }) => {
         <SEO title="Registration" />
         {/* <div className=" h-screen"> */}
         <BackgroundImage
-          className="flex flex-col row-gap-10 items-center justify-center text-white h-400px"
+          className="flex flex-col row-gap-10 items-center justify-center text-white h-400px relative"
           fluid={waiverBackground.childImageSharp.fluid}
         >
           <h1 className="leading-none mt-20  font-semibold">Registration</h1>
+          <div className="flex fixed top-0 right-0 font-semibold mt-4 mr-4 col-gap-4">
+            <Link activeClassName="underline" to="/">
+              Home
+            </Link>
+            <Link to="/registration">Registration</Link>
+          </div>
         </BackgroundImage>
         {/* Main Content */}
-        <div className="grid">
+        <div className="grid py-20">
           {/* Inner, "floating" container */}
-          <div className="m-20 lg:w-1/2 justify-self-center">
-            <section className="text-3xl mt-10">
-              <h2 className="text-5xl text-center font-semibold">
-                Welcome to the 2020-21 Fatbike season! Registration is now open.
-              </h2>
-              <div className="mt-5">
-                <H3>Our membership fees are as follows:</H3>
-                <ul className="list-disc ml-16">
-                  <Li>Single Fatbike $75.00</Li>
-                  <Li>Family Fatbike $120.00</Li>
-                  <Li>Single Snowshoe User $20.00</Li>
-                  <Li>Snowshoe Family $40.00</Li>
-                </ul>
-              </div>
-            </section>
-            <section className="text-3xl mt-10">
-              <H3>Registering for the season is a simple two step process.</H3>
-              <ol className="ml-16">
-                <Li>
-                  <Emph>Step 1:</Emph> E-Transfer the annual fee to{" "}
-                  <ExternalLink href="mailto:cedarbaytrailcompany@gmail.com">
-                    {/* Hidden space allows for line breaking of email address on smaller screens */}
-                    <span className="">
-                      cedarbaytrailcompany<span className="lg:hidden"> </span>
-                      @gmail.com
-                    </span>
-                  </ExternalLink>
-                </Li>
-                <Li>
-                  <Emph>Step 2:</Emph> Sign the release{" "}
-                  <ExternalLink href="https://app.waiversign.com/e/5f995bf516eb010019446497/doc/5f99742ad56fea00190cafa3?event=none">
-                    over at waiversign
-                  </ExternalLink>
-                  <span className="text-red-600">*</span> or{" "}
-                  <ExternalLink href="https://app.waiversign.com/e/5f995bf516eb010019446497/doc/5f99742ad56fea00190cafa3?event=none">
-                    download the waiver form
-                  </ExternalLink>
-                  , sign, scan and send it to{" "}
-                  <ExternalLink href="mailto:cedarbaytrailcompany@gmail.com">
-                    {/* Hidden space allows for line breaking of email address on smaller screens */}
-                    <span className="">
-                      cedarbaytrailcompany<span className="lg:hidden"> </span>
-                      @gmail.com
-                    </span>
-                  </ExternalLink>
-                </Li>
-                <Li>
-                  <Emph>Step 3:</Emph> Hit the Trails!
-                </Li>
-              </ol>
-              <div className="mt-12">
-                <span className="text-red-600">*</span>
-                <span className="italic">
-                  When completing the waiver you must tap on the "Initial" or
-                  "signature" boxes to auto populate these areas.
-                </span>
-              </div>
-            </section>
-            {/* <div>
+          {/* <div className="m-20 lg:w-1/2 justify-self-center"> */}
+          <PageSection className="mb-20">
+            <h2 className="text-5xl text-center font-semibold">
+              Welcome to the 2020-21 Fatbike season! Registration is now open.
+            </h2>
+          </PageSection>
+          <PageSection className="text-3xl bg-gray-500 text-white py-10">
+            <div className="mt-5">
+              <H3>Our membership fees are as follows:</H3>
+              <ul className="list-disc ml-16">
+                <Li>Single Fatbike $75.00</Li>
+                <Li>Family Fatbike $120.00</Li>
+                <Li>Single Snowshoe User $20.00</Li>
+                <Li>Snowshoe Family $40.00</Li>
+              </ul>
+            </div>
+          </PageSection>
+          <PageSection className="text-3xl py-10 ">
+            <H3>Registering for the season is a simple two step process.</H3>
+            <ol className="ml-16">
+              <Li>
+                <Emph>Step 1:</Emph> E-Transfer the annual fee to{" "}
+                <ExternalLink href="mailto:cedarbaytrailcompany@gmail.com">
+                  {/* Hidden space allows for line breaking of email address on smaller screens */}
+                  <span className="">
+                    cedarbaytrailcompany<span className="lg:hidden"> </span>
+                    @gmail.com
+                  </span>
+                </ExternalLink>
+              </Li>
+              <Li>
+                <Emph>Step 2:</Emph> Sign the release{" "}
+                <ExternalLink href="https://app.waiversign.com/e/5f995bf516eb010019446497/doc/5f99742ad56fea00190cafa3?event=none">
+                  over at waiversign
+                </ExternalLink>
+                .<span className="text-red-600">*</span> <br />
+                <Emph>or</Emph>
+                <ExternalLink href="https://app.waiversign.com/e/5f995bf516eb010019446497/doc/5f99742ad56fea00190cafa3?event=none">
+                  Download and print the waiver form
+                </ExternalLink>
+                , sign it, scan it and send it to{" "}
+                <ExternalLink href="mailto:cedarbaytrailcompany@gmail.com">
+                  {/* Hidden space allows for line breaking of email address on smaller screens */}
+                  <span className="">
+                    cedarbaytrailcompany<span className="lg:hidden"> </span>
+                    @gmail.com
+                  </span>
+                </ExternalLink>
+              </Li>
+              <Li>
+                <Emph>
+                  Step 3{" "}
+                  <span className="font-normal">
+                    (We lied, there's one more step!)
+                  </span>
+                  :{" "}
+                </Emph>{" "}
+                Go hit the trails
+              </Li>
+            </ol>
+            <div className="mt-12">
+              <span className="text-red-600">*</span>
+              <span className="italic">
+                When completing the online waiver you must tap on the "Initial"
+                or "Signature" boxes to auto populate these areas.
+              </span>
+            </div>
+          </PageSection>
+          {/* <div>
               <ButtonLink
                 href="https://app.waiversign.com/e/5f995bf516eb010019446497/doc/5f99742ad56fea00190cafa3?event=none"
                 text="Online Waiver"
                 className=""
               />
             </div> */}
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </Layout>
