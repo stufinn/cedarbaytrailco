62c5404e9d84e56fd97c9eed458db4da8dd107b0
Stu Finn <stu@borealisweb.ca>
2020-12-29 19:38:25 -0600

    Update registration layout

--- web/src/pages/register.js
+++ web/src/pages/register.js
@@ -13,7 +13,7 @@ const H3 = ({ children, className }) => (
   </h3>
 )
 
-const Li = ({ children, className = "" }) => (
+const Li = ({ children, className = "grid" }) => (
   <li className={`mt-2 mb-5 ${className}`}>{children}</li>
 )
 
@@ -33,13 +33,16 @@ const ExternalLink = ({
 )
 
 const Emph = ({ children, className = "" }) => (
-  <div className={`font-bold mb-2 ${className}`}>{children}</div>
+  <div className={`font-bold mb-2 ${className}   mt-10 text-4xl p-3`}>
+    {children}
+    <div className="border-b-2 mt-1 w-full max-w-xl"></div>
+  </div>
 )
 
 const PageSection = ({ children, className = "" }) => (
   <div className={` ${className}`}>
-    <div className={`flex mx-10 justify-center`}>
-      <div className="lg:w-1/2">{children}</div>
+    <div className={`flex mx-10  lg:mx-32 justify-center`}>
+      <div className="">{children}</div>
     </div>
   </div>
 )
@@ -68,7 +71,7 @@ const Register = ({ data }) => {
           {/* <div className="m-20 lg:w-1/2 justify-self-center"> */}
           <PageSection className="mb-20">
             <h2 className="text-5xl text-center font-semibold">
-              Welcome to the 2020-21 Fatbike & Snowshoe season!
+              Welcome to the 2020-2021 Fatbike &amp; Snowshoe season!
             </h2>
             <h2 className="text-5xl text-center font-semibold pt-5">
               Registration is now open.
@@ -76,7 +79,7 @@ const Register = ({ data }) => {
           </PageSection>
           <PageSection className="text-3xl bg-gray-500 text-white py-10">
             <div className="mt-5">
-              <H3>Our membership fees are as&nbsp;follows:</H3>
+              <H3>Our 2020-2021 membership fees are as&nbsp;follows:</H3>
               <ul className="list-disc ml-16">
                 <Li>Single Fatbike - $75.00</Li>
                 <Li>Family Fatbike - $120.00</Li>
@@ -87,9 +90,9 @@ const Register = ({ data }) => {
           </PageSection>
           <PageSection className="text-3xl py-10 ">
             <H3>Registering for the season is a simple two step process.</H3>
-            <ol className="ml-16">
+            <ol className="">
               <Li>
-                <Emph>Step 1:</Emph>{" "}
+                <Emph>Step&nbsp;1</Emph>{" "}
                 <IndentedItem>
                   E-Transfer the annual fee to{" "}
                   <ExternalLink href="mailto:cedarbaytrailcompany@gmail.com">
@@ -109,7 +112,7 @@ const Register = ({ data }) => {
                     over at waiversign
                   </ExternalLink>
                   .<span className="text-red-600">*</span> <br />
-                  <Emph className="ml-2">or</Emph>
+                  <div className="ml-2 font-bold my-5">or</div>
                   <ExternalLink href="#">
                     Download and print the waiver form
                   </ExternalLink>
@@ -122,25 +125,24 @@ const Register = ({ data }) => {
                     </span>
                   </ExternalLink>
                 </IndentedItem>
+                <div className="mt-12 text-2xl">
+                  <span className="text-red-600 ">*</span>
+                  <span className="italic">
+                    When completing the online waiver you must tap on the
+                    "Initial" or "Signature" boxes to auto populate these areas.
+                  </span>
+                </div>
               </Li>
               <Li>
-                <Emph>
-                  Step 3{" "}
-                  <span className="font-normal">
-                    (We lied, there's one more step!)
-                  </span>
-                  :{" "}
-                </Emph>{" "}
-                <IndentedItem>Go hit the trails.</IndentedItem>
+                <Emph>Step 3 </Emph>{" "}
+                <IndentedItem>
+                  <p>Ok we lied, there's one more step:</p>
+                  <p className="mt-10 ">
+                    <span className="italic">Go hit those trails!</span> 🚲 ❄
+                  </p>
+                </IndentedItem>
               </Li>
             </ol>
-            <div className="mt-12">
-              <span className="text-red-600">*</span>
-              <span className="italic">
-                When completing the online waiver you must tap on the "Initial"
-                or "Signature" boxes to auto populate these areas.
-              </span>
-            </div>
           </PageSection>
           {/* <div>
               <ButtonLink
