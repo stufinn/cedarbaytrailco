7045babaa9b97f7480281d75780aab20697f22e7
Stu Finn <stu@borealisweb.ca>
2020-12-31 15:02:50 -0600

    nav text size changes

--- web/src/components/navbar/navbar.js
+++ web/src/components/navbar/navbar.js
@@ -77,7 +77,7 @@ const NavBar = () => {
   ]
 
   return (
-    <nav className=" absolute z-10 grid grid-flow-col gap-x-3 w-full text-white   md:text-xl mt-2 pr-4 md:pr-8 justify-end font-bold uppercase tracking-wide ">
+    <nav className=" absolute z-10 grid grid-flow-col gap-x-3 w-full text-white text-xl md:text-2xl mt-2 pr-4 md:pr-8 justify-end font-bold uppercase tracking-wide ">
       {/* navbar logo */}
       {/* desktop nav */}
       {/* <NavGridContainer className=" bg-none h-64"> */}
