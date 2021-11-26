dc10f1a1cfbb6e34e645666cb9b5e3c621c1d3e3
Stu Finn <stu@borealisweb.ca>
2021-11-26 07:17:29 -0600

    Initial work on trail conditions

--- web/src/pages/trail-conditions.js
+++ web/src/pages/trail-conditions.js
@@ -4,11 +4,21 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 
+const trails = [
+  {
+    name: "Candy Land",
+    status: "red",
+    notes: "Some notes",
+    updated: "November 16, 10:21pm",
+  },
+]
+
 const TrailConditions = () => (
   <Layout>
     <SEO title="Trail Conditions" />
     <div className="">
       <h1>Trail Conditions</h1>
+      <div></div>
 
       <Link to="/" className="settings__mainButton success__button">
         Back to Home Page
